aac4027905991ba61a36406184942c2d29bc49f2
Sathish G <57170450+gsatkani@users.noreply.github.com>
2020-05-11 11:14:26 +0530

    Add files via upload

---
+++ src/com/servlet/FiistServlet.java
@@ -0,0 +1,52 @@
+package com.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/FiistServlet")
+public class FiistServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FiistServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session=request.getSession(false);
+		if(session.getAttribute("username")!=null) {
+			
+			response.getWriter().println((String)session.getAttribute("username"));
+			response.getWriter().println("<hr><a href='SecondServlet'>Click to SecondServlet</a>");
+			
+		}
+		else {
+			request.getRequestDispatcher("login-admin-page.jsp").forward(request,response);
+		}
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
